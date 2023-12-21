a54ec97688aac56e74e944b4ab01a331a8ac8b1a
Faiyaz Kazi <faiyazk1012@gmail.com>
2023-12-21 01:57:54 -0500

    updating GUI

--- VigenereCypher.java
+++ VigenereCypher.java
@@ -1,6 +1,10 @@
 
 //importing entire java.util library for future reference
-import java.util.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Represents the Vigenere Cypher
@@ -10,25 +14,80 @@ import java.util.*;
  * @since Friday, September 16, 2022
  */
 public class VigenereCypher {
+
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Vigenere Cipher");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(400, 250);
+            frame.setLayout(new BorderLayout());
 
-        // creating scanner object for future use
-        Scanner in = new Scanner(System.in);
+            JPanel titlePanel = new JPanel();
+            titlePanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+            JLabel titleLabel = new JLabel("Shh... Let's Encrypt!");
+            titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
+            titlePanel.add(titleLabel);
 
-        // input from user for plaintext
-        System.out.print("Enter message: ");
-        String plaintext = in.nextLine();
+            JPanel mainPanel = new JPanel();
+            mainPanel.setLayout(new GridBagLayout());
 
-        // input from user for keyword
-        System.out.print("Enter keyword: ");
-        String keyword = in.nextLine();
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.insets = new Insets(5, 5, 5, 5);
 
-        // capitalizing and removing all non-letters from plaintext and keyword
-        plaintext = plaintext.replaceAll("[^a-zA-Z]", "").toUpperCase();
-        keyword = keyword.replaceAll("[^a-zA-Z]", "").toUpperCase();
+            JLabel plainLabel = new JLabel("Enter message:");
+            JTextField plaintextField = new JTextField(20);
+            JLabel keywordLabel = new JLabel("Enter keyword:");
+            JTextField keywordField = new JTextField(20);
 
-        // calling the encryption of the plaintext using the keyword
-        encryption(plaintext, keyword);
+            JButton encryptButton = new JButton("Encrypt");
+
+            JLabel resultLabel = new JLabel();
+
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            mainPanel.add(plainLabel, gbc);
+
+            gbc.gridx = 1;
+            mainPanel.add(plaintextField, gbc);
+
+            gbc.gridx = 0;
+            gbc.gridy = 1;
+            mainPanel.add(keywordLabel, gbc);
+
+            gbc.gridx = 1;
+            mainPanel.add(keywordField, gbc);
+
+            gbc.gridx = 0;
+            gbc.gridy = 2;
+            gbc.gridwidth = 2;
+            gbc.anchor = GridBagConstraints.CENTER;
+            mainPanel.add(encryptButton, gbc);
+
+            frame.add(titlePanel, BorderLayout.NORTH);
+            frame.add(mainPanel, BorderLayout.CENTER);
+
+            encryptButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String plaintext = plaintextField.getText();
+                    String keyword = keywordField.getText();
+
+                    String encryptedMessage = encryption(plaintext, keyword);
+
+                    mainPanel.removeAll();
+                    mainPanel.add(resultLabel, gbc);
+                    resultLabel.setText("Encrypted Message: " + encryptedMessage);
+                    animateFadeIn(resultLabel);
+
+                    frame.revalidate();
+                    frame.repaint();
+                }
+            });
+
+            frame.setLocationRelativeTo(null);
+
+            frame.setVisible(true);
+        });
     }
 
     /**
@@ -37,7 +96,7 @@ public class VigenereCypher {
      * @param plaintext The message to be encrypted.
      * @param keyword   The key to encrypting the message.
      */
-    public static void encryption(String plaintext, String keyword) {
+    public static String encryption(String plaintext, String keyword) {
 
         // creating char objects for future use
         char[] plaintextArray = plaintext.toCharArray(); // Converting plaintext to char array for future reference
@@ -65,5 +124,30 @@ public class VigenereCypher {
 
         // outputting encrypted message
         System.out.println("Encrypted Message: " + String.valueOf(plaintextEncrypted));
+
+        // returning encrypted message
+        return String.valueOf(plaintextEncrypted);
+    }
+
+    /**
+     * Animate fade-in effect for a JLabel.
+     *
+     * @param label The JLabel to animate.
+     */
+    private static void animateFadeIn(JLabel label) {
+        javax.swing.Timer timer = new javax.swing.Timer(20, new ActionListener() {
+            private float alpha = 0f;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                alpha += 0.05f;
+                if (alpha >= 1.0f) {
+                    alpha = 1.0f;
+                    ((javax.swing.Timer) e.getSource()).stop();
+                }
+                label.setForeground(new Color(0, 0, 0, alpha));
+            }
+        });
+        timer.start();
     }
 }
